9991d552a0f67fc1ef9719c89e2bcbacd8572c44
Alex Lustig <exoxander15@gmail.com>
2023-08-14 15:56:51 -0600

    home page works, looks fine for now

--- simplepages/pages/models.py
+++ simplepages/pages/models.py
@@ -1,21 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class profile(models.Model):
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profileimage = models.ImageField(upload_to="profileimages", default="NotFound.jpg")
 
-    user = models.ForeignKey(models.User, on_delete=models.CASCADE)
-    profileimage = models.ImageField(upload_to="profileimages", default="profileimages/default.jpg")
+    def __str__(self):
+        return self.user.username
 
-class post(models.Model):
+class Post(models.Model):
     postname = models.CharField(max_length=20)
     postdescription = models.CharField(max_length=2048)
 
-    user = models.ForeignKey(models.User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
-    postimage = models.ImageField(upload_to="postimages", default="postimages/default.jpg")
+    postimage = models.ImageField(upload_to="postimages", default="NotFound.jpg")
 
 
 class comment(models.Model):
-    post = models.ForeignKey(post, on_delete=models.CASCADE)
-    user = models.ForeignKey(models.User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     commentdata = models.CharField(max_length=128)
